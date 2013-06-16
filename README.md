7cb62d7e46bc220a5d5b81466f2c6ebea4551dfe
Kazuhiro Homma <kazu.homma@gmail.com>
2013-06-16 11:03:51 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Acme::Tategaki - It's new $module
+
+# SYNOPSIS
+
+    use Acme::Tategaki;
+
+# DESCRIPTION
+
+Acme::Tategaki is ...
+
+# LICENSE
+
+Copyright (C) Kazuhiro Homma.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kazuhiro Homma <kazu.homma@gmail.com>
---
+++ lib/Acme/Tategaki.pm
@@ -0,0 +1,39 @@
+package Acme::Tategaki;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::Tategaki - It's new $module
+
+=head1 SYNOPSIS
+
+    use Acme::Tategaki;
+
+=head1 DESCRIPTION
+
+Acme::Tategaki is ...
+
+=head1 LICENSE
+
+Copyright (C) Kazuhiro Homma.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kazuhiro Homma E<lt>kazu.homma@gmail.comE<gt>
+
+=cut
+
