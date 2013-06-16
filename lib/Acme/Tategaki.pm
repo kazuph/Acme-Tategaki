955b64d9704dee09b2f6274421d4a183d273b95a
Kazuhiro Homma <kazu.homma@gmail.com>
2013-06-16 14:06:27 +0900

    一旦の完成

--- lib/Acme/Tategaki.pm
+++ lib/Acme/Tategaki.pm
@@ -3,15 +3,37 @@ use 5.008005;
 use strict;
 use warnings;
 use utf8;
-# use Moo;
+use Array::Transpose;
+use List::Util qw(max);
+use Encode qw/decode_utf8 encode_utf8/;
 
 our $VERSION = "0.01";
 
+my @punc             = qw(、 。 ， ．);
+my @horizontal_words = qw(ー 「 」 → ↑ ← ↓ ＝ …);
+my @vertical_words   = qw(｜ ¬ ∟ ↓ → ↑ ← ॥ ：);
+my %replace_words = map {$horizontal_words[$_] => $vertical_words[$_]} (0..$#horizontal_words);
+
 sub tategaki {
     my $self = shift;
-    my $text = shift || 'hoge';
+    my @text = @_;
+    my $text = join '　', map{decode_utf8 $_} @text;
 
-    print $text;
+    while (my($key, $value) = each %replace_words) {
+        $text =~ s/$key/$value/g;
+    }
+
+    @text = split /\s/, $text;
+    my $max_lengh =  max map {length $_} @text;
+    @text = map{$_ . ('　' x ($max_lengh - length $_))} @text;
+    @text = map {[split //, $_]} @text;
+    @text = transpose([@text]);
+    @text = map {join '　', reverse @$_} @text;
+    print encode_utf8 $_, "\n" for @text;
+}
+
+if ( __FILE__ eq $0 ) {
+    tategaki();
 }
 
 1;
